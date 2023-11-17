33026b12c9c9a27ca5c927061d7891c7bbdbe64a
Pranta Saha (prs527) <prs527@mail.usask.ca>
2023-11-17 10:32:23 -0600

    BackGround Color Project is done

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import HelloReact from "./Journey";
+
+function App() {
+  return (
+    <>    <h1>React Journey</h1>
+    <HelloReact/> </>
+
+  );
+}
+
+export default App;
---
+++ myapp/src/Journey.js
@@ -0,0 +1,6 @@
+// Components should be made using Capital Letters
+
+function HelloReact(){
+    return(<h1>Chaa Khaw ar Poro</h1>)
+}
+export default HelloReact
